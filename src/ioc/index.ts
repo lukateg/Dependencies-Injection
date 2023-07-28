5d1b1cc13fc6ea4b1842d3f77111f4b39d4445bf
Luka <ltegeltija@griddynamics.com>
2023-07-28 11:54:31 +0200

    Rewrite dependency injection

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,20 @@
-import IoCContainer from 'ioc-lite';
+import IoCContainer from "ioc-lite";
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from "../services/logger";
+import { HTTP } from "../services/http";
+import { Users } from "../services/users";
 
-export const createIoCContainer = () =>  {
+export const createIoCContainer = () => {
   const ioc = new IoCContainer();
   // you can register some resources right now below...
 
+  ioc.registerClass("logger", Logger);
+  ioc.registerClass("http", HTTP);
+  ioc.register("users", Users);
+
   return ioc;
 };
+
+export const logger = createIoCContainer().resolve("logger");
+export const http = createIoCContainer().resolve("http");
+export const users = createIoCContainer().resolve("users");
